5cef7ff2d83383074bfed7c0f81ac2c6e13591ec
FredrikStorm <frpe23af@student.cbs.dk>
2024-11-21 16:25:39 +0100

    SPa 2

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const express = require('express');
-
-
-
-
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
-// Middleware to parse JSON bodies
-app.use(express.json());
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Sample route
+// Serve welcome.html for the root URL
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.sendFile(path.join(__dirname, 'public', 'html', 'welcome.html'));
 });
 
 // Start the server
